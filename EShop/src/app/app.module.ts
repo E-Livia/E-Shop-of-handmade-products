db10da313c5b5968e7956f017b8630a2615c62a4
E-Livia <livia.popa.3@gmail.com>
2023-03-26 00:42:55 +0200

    clean-up

--- EShop/src/app/app.module.ts
+++ EShop/src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { ro_RO } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
-import ro from '@angular/common/locales/ro';
+import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
-registerLocaleData(ro);
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -22,9 +23,10 @@ registerLocaleData(ro);
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NzLayoutModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: ro_RO }],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- EShop/src/app/features/main-page/main-page.module.ts
+++ EShop/src/app/features/main-page/main-page.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageComponent } from './main-page/main-page.component';
 
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
   imports: [
     CommonModule,
     MainPageRoutingModule,
-    NzButtonModule
+    NzButtonModule,
+    NzRadioModule,
+    FormsModule,
   ]
 })
 export class MainPageModule { }
